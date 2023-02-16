73964647d5a8de386ae067abce79a4f1482f9e2d
Seb Barnes <sebastianjbarnes@gmail.com>
2023-02-15 23:12:10 -0500

    Update GeekTextApplication.java

--- GeekText/src/main/java/com/Group3/GeekText/GeekTextApplication.java
+++ GeekText/src/main/java/com/Group3/GeekText/GeekTextApplication.java
@@ -3,7 +3,7 @@ package com.Group3.GeekText;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+//Test
 
 @SpringBootApplication(scanBasePackages = {"com.Group3.GeekText"})
 @EnableJpaRepositories(basePackages = {"com.Group3.GeekText"})
