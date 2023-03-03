e24bb8ae619400bb3dee1a062b91de1582235f74
Seb Barnes <sebastianjbarnes@gmail.com>
2023-03-02 22:45:08 -0500

    Added GET by specific ID
    Added the ability to GET specific ID's by adding to the Service Class for Profiles.

--- GeekText/src/main/java/com/Group3/GeekText/controllers/ProfilesController.java
+++ GeekText/src/main/java/com/Group3/GeekText/controllers/ProfilesController.java
@@ -2,6 +2,8 @@ package com.Group3.GeekText.controllers;
 
 import com.Group3.GeekText.repositories.ProfilesRepository;
 import com.Group3.GeekText.entities.Profile;
+import com.Group3.GeekText.services.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
 public class ProfilesController {
 
+    @Autowired
+    ProfileService profileService;
 
     private final ProfilesRepository profilesRepository;
 
@@ -27,11 +32,13 @@ public class ProfilesController {
         return profilesRepository.findAll();
     }
 
-//    @GetMapping("/profiles/{id}")
-//    public ResponseEntity<Profile> findProfileID(long id) {
-//        Profile profile = profilesRepository.findByID(id);
-//        return ResponseEntity.ok(profile);
-//    }
+   @GetMapping("/profiles/{id}")
+    public ResponseEntity<Profile> getById(@PathVariable long id) {
+       Optional<Profile> profile = profileService.getById(id);
+
+           return new ResponseEntity<>(profile.get(), HttpStatus.OK);
+
+       }
 
     @GetMapping("/helloWorld")
     public String helloWorld(){
