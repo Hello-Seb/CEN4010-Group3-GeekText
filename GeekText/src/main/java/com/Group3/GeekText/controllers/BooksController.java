e12366745f864e49f1f21a86c86e0bc584156723
Vale-Ay <102432143+Vale-Ay@users.noreply.github.com>
2023-03-11 19:51:39 -0500

    still attempting genre

--- GeekText/src/main/java/com/Group3/GeekText/controllers/BooksController.java
+++ GeekText/src/main/java/com/Group3/GeekText/controllers/BooksController.java
@@ -1,32 +1,47 @@
 package com.Group3.GeekText.controllers;
 
 import com.Group3.GeekText.entities.Books;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.Group3.GeekText.repositories.BooksRepository;
-import com.Group3.GeekText.entities.Books;
+import com.Group3.GeekText.services.BooksService;
+
+
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/books")
 public class BooksController {
 
-
-    private final BooksRepository booksRepository;
-
-    public BooksController(BooksRepository booksRepository) {
-        this.booksRepository = booksRepository;
+    @Autowired
+    private BooksService booksService;
+    /*@Autowired
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }*/
+    @GetMapping
+    public List<Books> getBook(@RequestParam(required = false) String bookGenre) {
+        if (bookGenre == null){
+            return booksService.getBooks();
+        }
+        else{
+            List<Books> genreSearch = booksService.findByGenre(bookGenre);
+            return genreSearch;
+        }
     }
-
 
     @GetMapping("/getAllBooks")
-    public List<Books> getAllBooks() {
-        return booksRepository.findAll();
+    public List<Books> getBooks() {
+        return booksService.findAll();
     }
-
     @PostMapping(value = "/addBook")
     public Books createBook(@RequestBody Books books){
         return books;
     }
 
+    @GetMapping("/genre/{genre}")
+    public List<Books> getBooksByGenre(@PathVariable("genre") String bookGenre){
+        return booksService.getBooksByGenre(bookGenre);
+    }
 }
