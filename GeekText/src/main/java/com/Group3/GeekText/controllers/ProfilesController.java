73c6a5b2d8ea5539841223cce62627b4b7db69ce
Seb Barnes <sebastianjbarnes@gmail.com>
2023-03-15 22:36:55 -0400

    Removed redundant code from ProfileController

--- GeekText/src/main/java/com/Group3/GeekText/controllers/ProfilesController.java
+++ GeekText/src/main/java/com/Group3/GeekText/controllers/ProfilesController.java
@@ -1,15 +1,8 @@
 package com.Group3.GeekText.controllers;
 import com.Group3.GeekText.repositories.ProfilesRepository;
-import com.Group3.GeekText.repositories.CreditCardRepository;
 import com.Group3.GeekText.entities.Profile;
-import com.Group3.GeekText.entities.CreditCard;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
@@ -29,12 +22,6 @@ public class ProfilesController {
     @GetMapping("/profiles/username/{username}")
     public List<Profile> getByUsername(@PathVariable String username) {return profilesRepository.findByUsername(username); }
 
-    @GetMapping("/profiles/name/{username}")
-    public ResponseEntity<String> username(@PathVariable String username) {
-        String name = profilesRepository.findNameByUsername(username);
-        return new ResponseEntity<>(name, HttpStatus.OK);
-
-    }
 
     @PostMapping("/profiles")
     public void postProfile(@RequestBody Profile profile) {
