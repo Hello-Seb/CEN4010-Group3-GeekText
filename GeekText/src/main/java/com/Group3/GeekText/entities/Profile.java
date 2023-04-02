de12bd9700ab8f6e48bed105e529adcb0b546b57
Seb Barnes <sebastianjbarnes@gmail.com>
2023-04-02 17:49:02 -0400

    Added Initial Comments
    More comments to come, did some basic cleanup

--- GeekText/src/main/java/com/Group3/GeekText/entities/Profile.java
+++ GeekText/src/main/java/com/Group3/GeekText/entities/Profile.java
@@ -4,28 +4,36 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 @Entity
+//This entity is connected to the Profiles table in the SQL Database
 @Table(name = "Profiles")
 public class Profile {
 
+    //Setting Id generation type so that SQL database is able to generate unique IDs upon creation of entity
     @Id
     @JsonBackReference
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    //Assigning variables to each column in the Profiles table
+    //ProfileID is mandatory so nullable is set to false
     @Column(name = "ProfileID", nullable = false)
     private long profileID;
 
     @Column(name = "Username")
     private String username;
+
     @Column(name = "Password")
     private String password;
+
     @Column(name = "Name")
     private String name;
+
     @Column(name = "EmailAddress")
     private String emailAddress;
+
     @Column(name = "HomeAddress")
     private String homeAddress;
 
-
+    //Setter and getter methods for all of the above variables
     public long getProfileID() {return profileID;}
     public void setProfileID(long profileID) {
         this.profileID = profileID;
@@ -55,14 +63,8 @@ public class Profile {
     public String getEmailAddress() {
         return emailAddress;
     }
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress;}
 
-    public String getHomeAddress() {
-        return homeAddress;
-    }
-    public void setHomeAddress(String homeAddress) {
-        this.homeAddress = homeAddress;
-    }
+    public String getHomeAddress() {return homeAddress;}
+    public void setHomeAddress(String homeAddress) {this.homeAddress = homeAddress;}
 }
